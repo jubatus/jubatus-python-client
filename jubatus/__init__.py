f826f12030bd04106622a6867b058ce68e6b1c28
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-09-25 17:22:51 +0900

    remove generated clients

--- jubatus/__init__.py
+++
@@ -1,8 +0,0 @@
-
-__all__ = ["classifier", "regression", "recommender", "stat", "graph"]
-
-from jubatus.classifier.client import classifier as Classifier
-from jubatus.regression.client import regression as Regression
-from jubatus.recommender.client import recommender as Recommender
-from jubatus.stat.client import stat as Stat
-from jubatus.graph.client import graph as Graph
