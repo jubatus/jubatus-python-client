4371b8746ba16393cfbc017a089d34f860bb3414
Yuya Unno <unnonouno@gmail.com>
2013-07-03 11:10:03 +0900

    Add exceptions to __init__

--- jubatus/common/__init__.py
+++ jubatus/common/__init__.py
@@ -1,2 +1,2 @@
 from types import TInt, TFloat, TBool, TString, TRaw, TNullable, TList, TMap, TTuple, TUserDef, TObject
-from client import Client
+from client import Client, TypeMismatch, UnknownMethod
