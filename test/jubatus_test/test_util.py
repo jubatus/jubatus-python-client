816cd43cd0fd01a296ddde6e6bf6338400777020
Yuya Unno <unnonouno@gmail.com>
2013-09-03 15:27:40 +0900

    Check if server is alive before test

--- test/jubatus_test/test_util.py
+++ test/jubatus_test/test_util.py
@@ -9,19 +9,25 @@ from msgpackrpc.error import *
 
 class TestUtil:
   @staticmethod
+  def check_server(port):
+    # For Python clients, we need to generate instance for each iteration.
+    cli = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", port))
+    try:
+      cli.call("dummy")
+      raise Exception("dummy rpc succeeded")
+    except RPCError, e:
+      if e.args[0] == 1: # "no such method"
+        return True         # ... means server is fully up
+    return False
+
+  @staticmethod
   def wait_server(port):
     sleep_time = 1000 # usec
     # 1000 * \sum {i=0..9} 2^i = 1024000 micro sec = 1024 ms
     for i in range(10):
-        # For Python clients, we need to generate instance for each iteration.
-        cli = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", port))
         time.sleep(sleep_time/1000000.0) # from usec to sec
-        try:
-            cli.call("dummy")
-            raise Exception("dummy rpc succeeded")
-        except RPCError, e:
-            if e.args[0] == 1: # "no such method"
-                return         # ... means server is fully up
+        if TestUtil.check_server(port):
+          return
         sleep_time *= 2;
     raise Exception("cannot connect")
 
@@ -30,9 +36,13 @@ class TestUtil:
     cmd = "juba" + name
     args = [cmd, "--rpc-port", str(port), "--configpath", config, "--thread", "100", "--datadir", "."]
     try:
+      if TestUtil.check_server(port):
+        raise Exception('Another server is already running')
       # use PIPE to surpress log messages of server processes
       proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       TestUtil.wait_server(port)
+      if proc.poll():
+        raise Exception('Cannot run server process')
       return proc
     except OSError as error:
       print 'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
