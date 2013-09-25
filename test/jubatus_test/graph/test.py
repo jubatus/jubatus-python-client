1c38d38566bd78450830cfc9b08604271dae7248
Yuya Unno <unno@preferred.jp>
2013-09-25 17:12:01 +0900

    Fix space size

--- test/jubatus_test/graph/test.py
+++ test/jubatus_test/graph/test.py
@@ -5,7 +5,7 @@ import json
 import msgpackrpc
 
 from jubatus.graph.client import Graph
-from jubatus.graph.types  import *
+from jubatus.graph.types    import *
 from jubatus.common import Datum
 
 from jubatus_test.test_util import TestUtil
@@ -15,68 +15,68 @@ port = 21005
 timeout = 10
 
 class GraphTest(unittest.TestCase):
-  def setUp(self):
-    self.config = {
-        "method": "graph_wo_index",
-        "parameter": {
-            "damping_factor": 0.9,
-            "landmark_num": 5
-        }
-    }
+    def setUp(self):
+        self.config = {
+            "method": "graph_wo_index",
+            "parameter": {
+                "damping_factor": 0.9,
+                "landmark_num": 5
+                }
+            }
 
-    TestUtil.write_file('config_graph.json', json.dumps(self.config))
-    self.srv = TestUtil.fork_process('graph', port, 'config_graph.json')
-    try:
-      self.cli = Graph(host, port, "name")
-    except:
-      TestUtil.kill_process(self.srv)
-      raise
+        TestUtil.write_file('config_graph.json', json.dumps(self.config))
+        self.srv = TestUtil.fork_process('graph', port, 'config_graph.json')
+        try:
+            self.cli = Graph(host, port, "name")
+        except:
+            TestUtil.kill_process(self.srv)
+            raise
 
-  def tearDown(self):
-    TestUtil.kill_process(self.srv)
+    def tearDown(self):
+        TestUtil.kill_process(self.srv)
 
-  def test_node_info(self):
-    edge_query = [["a", "b"], ["c", "d"], ["e", "f"]]
-    node_query = [["0", "1"], ["2", "3"]]
-    p = PresetQuery(edge_query, node_query)
-    in_edges = [0, 0]
-    out_edges = [0, 0]
-    Node(p, in_edges, out_edges)
+    def test_node_info(self):
+        edge_query = [["a", "b"], ["c", "d"], ["e", "f"]]
+        node_query = [["0", "1"], ["2", "3"]]
+        p = PresetQuery(edge_query, node_query)
+        in_edges = [0, 0]
+        out_edges = [0, 0]
+        Node(p, in_edges, out_edges)
 
-  def test_get_client(self):
-    self.assertTrue(isinstance(self.cli.get_client(), msgpackrpc.client.Client))
+    def test_get_client(self):
+        self.assertTrue(isinstance(self.cli.get_client(), msgpackrpc.client.Client))
 
-  def test_create_node(self):
-    nid = self.cli.create_node()
-    self.assertEqual(str(int(nid)), nid)
+    def test_create_node(self):
+        nid = self.cli.create_node()
+        self.assertEqual(str(int(nid)), nid)
 
-  def test_remove_node(self):
-    nid = self.cli.create_node()
-    self.assertEqual(self.cli.remove_node(nid), True)
+    def test_remove_node(self):
+        nid = self.cli.create_node()
+        self.assertEqual(self.cli.remove_node(nid), True)
 
-  def test_update_node(self):
-    nid = self.cli.create_node()
-    prop = {"key1":"val1", "key2":"val2"}
-    self.assertEqual(self.cli.update_node(nid, prop), True)
+    def test_update_node(self):
+        nid = self.cli.create_node()
+        prop = {"key1":"val1", "key2":"val2"}
+        self.assertEqual(self.cli.update_node(nid, prop), True)
 
-  def test_create_edge(self):
-    src = self.cli.create_node()
-    tgt = self.cli.create_node()
-    prop = {"key1":"val1", "key2":"val2"}
-    ei = Edge(prop, src, tgt)
-    eid = self.cli.create_edge(tgt, ei)
+    def test_create_edge(self):
+        src = self.cli.create_node()
+        tgt = self.cli.create_node()
+        prop = {"key1":"val1", "key2":"val2"}
+        ei = Edge(prop, src, tgt)
+        eid = self.cli.create_edge(tgt, ei)
 
-  def test_str(self):
-    self.assertEqual("node{property: {}, in_edges: [], out_edges: []}",
-                     str(Node({}, [], [])))
-    self.assertEqual("preset_query{edge_query: [], node_query: []}",
-                     str(PresetQuery([], [])))
-    self.assertEqual("edge{property: {}, source: src, target: tgt}",
-                     str(Edge({}, 'src', 'tgt')))
-    self.assertEqual("shortest_path_query{source: src, target: tgt, max_hop: 10, query: preset_query{edge_query: [], node_query: []}}",
-                     str(ShortestPathQuery('src', 'tgt', 10, PresetQuery([], []))))
+    def test_str(self):
+        self.assertEqual("node{property: {}, in_edges: [], out_edges: []}",
+                         str(Node({}, [], [])))
+        self.assertEqual("preset_query{edge_query: [], node_query: []}",
+                         str(PresetQuery([], [])))
+        self.assertEqual("edge{property: {}, source: src, target: tgt}",
+                         str(Edge({}, 'src', 'tgt')))
+        self.assertEqual("shortest_path_query{source: src, target: tgt, max_hop: 10, query: preset_query{edge_query: [], node_query: []}}",
+                         str(ShortestPathQuery('src', 'tgt', 10, PresetQuery([], []))))
 
 if __name__ == '__main__':
-  test_suite = unittest.TestLoader().loadTestsFromTestCase(GraphTest)
-  unittest.TextTestRunner().run(test_suite)
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(GraphTest)
+    unittest.TextTestRunner().run(test_suite)
 
--- test/jubatus_test/regression/test.py
+++ test/jubatus_test/regression/test.py
@@ -5,7 +5,7 @@ import json
 import msgpackrpc
 
 from jubatus.regression.client import Regression
-from jubatus.regression.types  import *
+from jubatus.regression.types import *
 from jubatus_test.test_util import TestUtil
 from jubatus.common import Datum
 
@@ -14,67 +14,65 @@ port = 21002
 timeout = 10
 
 class RegressionTest(unittest.TestCase):
-  def setUp(self):
-    self.config = {
-        "method": "PA",
-        "converter": {
-            "string_filter_types": {},
-            "string_filter_rules": [],
-            "num_filter_types": {},
-            "num_filter_rules": [],
-            "string_types": {},
-            "string_rules": [{"key": "*", "type": "str",  "sample_weight": "bin", "global_weight": "bin"}],
-            "num_types": {},
-            "num_rules": [{"key": "*", "type": "num"}]
-        },
-        "parameter": {
-            "sensitivity" : 0.1,
-            "regularization_weight" : 3.402823e+38
-        }
-    }
+    def setUp(self):
+        self.config = {
+            "method": "PA",
+            "converter": {
+                "string_filter_types": {},
+                "string_filter_rules": [],
+                "num_filter_types": {},
+                "num_filter_rules": [],
+                "string_types": {},
+                "string_rules": [{"key": "*", "type": "str", "sample_weight": "bin", "global_weight": "bin"}],
+                "num_types": {},
+                "num_rules": [{"key": "*", "type": "num"}]
+                },
+            "parameter": {
+                "sensitivity" : 0.1,
+                "regularization_weight" : 3.402823e+38
+                }
+            }
 
-    TestUtil.write_file('config_regression.json', json.dumps(self.config))
-    self.srv = TestUtil.fork_process('regression', port, 'config_regression.json')
-    try:
-      self.cli = Regression(host, port, "name")
-    except:
-      TestUtil.kill_process(self.srv)
-      raise
+        TestUtil.write_file('config_regression.json', json.dumps(self.config))
+        self.srv = TestUtil.fork_process('regression', port, 'config_regression.json')
+        try:
+            self.cli = Regression(host, port, "name")
+        except:
+            TestUtil.kill_process(self.srv)
+            raise
 
-  def tearDown(self):
-    TestUtil.kill_process(self.srv)
+    def tearDown(self):
+        TestUtil.kill_process(self.srv)
 
-  def test_get_client(self):
-    self.assertTrue(isinstance(self.cli.get_client(), msgpackrpc.client.Client))
+    def test_get_client(self):
+        self.assertTrue(isinstance(self.cli.get_client(), msgpackrpc.client.Client))
 
-  def test_get_config(self):
-    config = self.cli.get_config()
-    self.assertEqual(json.dumps(json.loads(config), sort_keys=True), json.dumps(self.config, sort_keys=True))
+    def test_get_config(self):
+        config = self.cli.get_config()
+        self.assertEqual(json.dumps(json.loads(config), sort_keys=True), json.dumps(self.config, sort_keys=True))
 
-  def test_train(self):
-    d = Datum({"skey1": "val1", "skey2": "val2", "nkey1": 1.0, "nkey2": 2.0})
-    data = [[1.0, d]]
-    self.assertEqual(self.cli.train(data), 1)
+    def test_train(self):
+        d = Datum({"skey1": "val1", "skey2": "val2", "nkey1": 1.0, "nkey2": 2.0})
+        data = [[1.0, d]]
+        self.assertEqual(self.cli.train(data), 1)
 
-  def test_estimate(self):
-    d = Datum({"skey1": "val1", "skey2": "val2", "nkey1": 1.0, "nkey2": 2.0})
-    data = [d]
-    result = self.cli.estimate(data)
+    def test_estimate(self):
+        d = Datum({"skey1": "val1", "skey2": "val2", "nkey1": 1.0, "nkey2": 2.0})
+        data = [d]
+        result = self.cli.estimate(data)
 
-  def test_save(self):
-    self.assertEqual(self.cli.save("regression.save_test.model"), True)
+    def test_save(self):
+        self.assertEqual(self.cli.save("regression.save_test.model"), True)
 
-  def test_load(self):
-    model_name = "regression.load_test.model"
-    self.cli.save(model_name)
-    self.assertEqual(self.cli.load(model_name), True)
+    def test_load(self):
+        model_name = "regression.load_test.model"
+        self.cli.save(model_name)
+        self.assertEqual(self.cli.load(model_name), True)
 
-  def test_get_status(self):
-    self.cli.get_status()
-
-
+    def test_get_status(self):
+        self.cli.get_status()
 
 if __name__ == '__main__':
-  test_suite = unittest.TestLoader().loadTestsFromTestCase(RegressionTest)
-  unittest.TextTestRunner().run(test_suite)
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(RegressionTest)
+    unittest.TextTestRunner().run(test_suite)
 
